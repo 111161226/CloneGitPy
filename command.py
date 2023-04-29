8557d50f4fd94544bd4e2fbd4df362d16dec1da8
111161226 <fanrin9@gmail.com>
2023-04-29 23:48:49 +0900

    addcommitfunc

--- command.py
+++ command.py
@@ -3,6 +3,19 @@ import git
 def excecute(cmd):
     if cmd[0] == "git":
         repo = git.Repo.init()
+        if cmd[1] == "add":
+            if len(cmd) >= 3 and cmd[2] != ".":
+                for i in range(2, len(cmd)):
+                    repo.index.add(cmd[i])
+        elif cmd[1] == "commit":
+            if cmd[2] == "-m" and len(cmd) >= 4:
+                mess = ""
+                for i in range(3, len(cmd)):
+                    mess+=cmd[i]
+            else:
+                mess = "done commit"
+            repo.index.commit(mess)
+        
         print("excecute ", cmd[0:])
 
     else:
